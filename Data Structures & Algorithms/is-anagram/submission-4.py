3bb5338e031c147301cb20a37f095c3c3fb2290c
Sapuan <sapuantalaspai@gmail.com>
2026-05-13 12:05:35 +0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = dict()
+        t_dict = dict() 
+
+        for i in s:
+            s_dict[i] = s_dict.get(i, 0) + 1
+
+        for i in t:
+            t_dict[i] = t_dict.get(i, 0) + 1
+
+        return s_dict == t_dict
