c870a6a2a7c91695e04861fadffcc3b2997586cd
Sapuan <sapuantalaspai@gmail.com>
2026-05-18 18:50:56 +0500

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,47 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # by row
+        for i in range(len(board)):
+            counter = dict()
+            
+            for j in range(len(board[i])):
+
+                if board[i][j].isdigit():
+                    counter[board[i][j]] = counter.get(board[i][j], 0) + 1
+
+                    if counter[board[i][j]] > 1:
+                        return False
+
+        # by column 
+        for i in range(len(board)):
+            k = 0
+            counter = dict()
+
+            for j in range(len(board[k])):
+
+                if board[k][i].isdigit():
+                    counter[board[k][i]] = counter.get(board[k][i], 0) + 1
+
+                    if counter[board[k][i]] > 1:
+                        return False 
+                k += 1
+
+        counter = dict()
+        for i in range(9):
+            for j in range(9):
+
+                num = board[i][j]
+                key = (i // 3, j // 3)
+
+                if num.isdigit():
+
+                    if key not in counter:
+                        counter[key] = set()
+
+                    if num in counter[key]:
+                        return False 
+
+                    counter[key].add(num)
+
+        return True
